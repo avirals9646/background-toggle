073efb9a81566ec4e4de9358804a73deed426029
avirals9646 <83738602+avirals9646@users.noreply.github.com>
2023-07-30 12:06:41 +0530

    modified according to the task

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+
+  const handleToggle = () => {
+    setIsDark(prevState => !prevState);
+  };
+
   return (
-    <div className="App">
+    <div className={`App ${isDark ? 'dark' : 'light'}`}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Background Toggle</h1>
+        <button onClick={handleToggle}>Toggle Background</button>
       </header>
     </div>
   );
